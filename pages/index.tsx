5100458925d4622a8053702d8829fda8db55303c
Paulo Dantas <paulo.roberto@hdn.digital>
2022-03-14 11:16:43 -0300

    Project - Zpotify

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+      <h1>Zpotify</h1>
+    </div>
+  )
+}
+
+export default Home
